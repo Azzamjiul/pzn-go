b0c93cd0de5d5fec6b920777150079593dccb318
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-01-27 19:14:56 +0700

    first commit

---
+++ belajar-golang-goroutines/channel_test.go
@@ -0,0 +1,61 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Eko Kurniawan Khannedy"
+		fmt.Println("Selesai Mengirim Data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Azzam Jihad Ulhaq"
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	select {
+	case data := <-channel1:
+		fmt.Println("Data dari channel 1", data)
+	case data := <-channel2:
+		fmt.Println("Data dari channel 2", data)
+	}
+}
