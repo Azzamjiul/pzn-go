5b02b4370e6e3a31c0f8775e96ca7db937557e37
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-01-27 19:34:19 +0700

    add TestMutex

--- belajar-golang-goroutines/race_condition_test.go
+++ belajar-golang-goroutines/race_condition_test.go
@@ -13,6 +13,21 @@ func TestRaceCondition(t *testing.T) {
 	for i := 0; i < 1000; i++ {
 		go func() {
 			for j := 0; j < 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
 				mutex.Lock()
 				x = x + 1
 				mutex.Unlock()
